3fdaf81caa7df98e73e1b23350f374a0b0d686cb
ly <3219747044@qq.com>
2024-10-18 16:29:52 +0800

    first commit

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+// 勾选setting 中的allow auto-merge
+public class TestCase {
+    @Test
+    public void testRight() {
+        Cal cal = new Cal();
+        assertEquals(cal.Add(1, 2), 3);
+        System.out.println("success");
+    }
+
+
+
+}
